6df1e1a82b66c343d27c302e95a51e9d4af32f9e
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2023-09-04 19:45:55 +0700

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,19 @@
+{ pkgs }: {
+  deps = [
+    pkgs.wget
+    pkgs.nodejs-19_x
+    pkgs.speedtest_cli
+    pkgs.zip
+    pkgs.unzip
+    pkgs.neofetch
+    pkgs.tesseract
+    pkgs.pm2
+    pkgs.wget
+    pkgs.libwebp
+    pkgs.speedtest-cli
+    pkgs.ffmpeg-full
+    pkgs.imagemagick
+    pkgs.sox
+    pkgs.curl
+  ];
+}
