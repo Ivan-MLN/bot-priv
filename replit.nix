aa70b336f4193c3bc20a22158f4baece488e81e0
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2023-09-05 07:33:43 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,22 +1,23 @@
 { pkgs }: {
-    deps = [              
-        pkgs.nodejs-16_x
-        pkgs.tesseract
-        pkgs.python
-	    	pkgs.nodePackages.typescript
-        pkgs.libuuid
-        pkgs.ffmpeg
-        pkgs.yarn
-        pkgs.imagemagick  
-        pkgs.wget
-        pkgs.sox
-        pkgs.curl
-        pkgs.zip
-			  pkgs.pm2
-        pkgs.git
-        pkgs.nodePackages.pm2
-    ];
-  env ={
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  deps = [              
+    pkgs.nodejs-16_x
+    pkgs.tesseract
+    pkgs.python
+    pkgs.nodePackages.typescript
+    pkgs.libuuid
+    pkgs.ffmpeg
+    pkgs.yarn
+    pkgs.imagemagick  
+    pkgs.wget
+    pkgs.sox
+    pkgs.curl
+    pkgs.zip
+    pkgs.pm2
+    pkgs.git
+    pkgs.nodePackages.pm2
+    pkgs.libsox-fmt-all
+  ];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.libuuid ];
   };
 }
